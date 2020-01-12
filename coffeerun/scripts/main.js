0539c79dcfd21e65da511712e590de85afc0f121
Iamanuan <gemma.mobu@icloud.com>
2020-01-12 13:54:56 +0800

    9-10

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -1,9 +1,15 @@
 (function(window){
   'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
-  // var myTruck = new Truck('ncc-1701',new DataStore());
-  var myTruck = new Truck('HAL',new DataStore());
+  var FormHandler = App.FormHandler;
+  var myTruck = new Truck('ncc-1701',new DataStore());
+  // var myTruck = new Truck('HAL',new DataStore());
   window.myTruck = myTruck;
+  var formHandler = new FormHandler(FORM_SELECTOR);
+  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  // formHandler.changeRangeHandler(); //当滑块滑动时显示其数值，并改变颜色
+  console.log(formHandler);
 })(window);
