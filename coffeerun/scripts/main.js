820fb814b2a19867fb69694f84304ec2b101cc5b
Iamanuan <gemma.mobu@icloud.com>
2020-01-19 11:15:11 +0800

    第14章

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -17,8 +17,13 @@
   checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
   var formHandler = new FormHandler(FORM_SELECTOR);
   formHandler.addSubmitHandler(function(data){
-    myTruck.createOrder.call(myTruck, data);
-    checkList.addRow.call(checkList, data);
+    return myTruck.createOrder.call(myTruck, data)
+      .then(function(){
+        checkList.addRow.call(checkList, data);
+      },function(){
+        alert('Server unreachable. Try again later.');
+      });
   });
   formHandler.addInputHandler(Validation.isCompanyEmail);
+  myTruck.printOrders(checkList.addRow.bind(checkList));
 })(window);
