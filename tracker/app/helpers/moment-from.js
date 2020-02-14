8ba0eccfee63189de53dfad96c18b5c6617f703c
Iamanuan <gemma.mobu@icloud.com>
2020-02-14 21:53:54 +0800

    第24章

--- tracker/app/helpers/moment-from.js
+++ tracker/app/helpers/moment-from.js
@@ -1,10 +1,10 @@
-import Ember from 'ember';
 import { helper } from '@ember/component/helper';
+import { htmlSafe } from '@ember/template'
 
 export function momentFrom(params){
   var time = window.moment(...params);
   var formatted = time.fromNow();
-  return Ember.String.htmlSafe(
+  return htmlSafe(
     '<span class="text-primary">'
     + formatted + '</span>'
   );
--- tracker/app/routes/sightings/new.js
+++ tracker/app/routes/sightings/new.js
@@ -1,4 +1,20 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default Route.extend({
+  model(){
+    return hash({
+      sighting: this.store.createRecord('sighting'),
+      cryptids: this.store.findAll('cryptid'),
+      witnesses: this.store.findAll('witness')
+    });
+  },
+  actions:{
+    willTransition(){
+      var sighting = this.get('controller.model.sighting');
+      if(sighting.get('hasDirtyAttributes')){
+        sighting.deleteRecord();
+      }
+    }
+  }
 });
